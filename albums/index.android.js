2fe374bf77c344bf5288465c9b5fdc3f6ba0f8ea
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-05-30 05:01:45 +0300

    Start to Develop!

---
+++ albums/index.android.js
@@ -0,0 +1,15 @@
+// Index.android.js - place code in here for android!!!
+
+
+// Import a library to help create a componenet
+import React from 'react';
+import ReactNative from 'react-native';
+
+// Create a Component
+const App = () => {
+  return(
+    <Text>Text Area</Text>
+  );
+}
+
+// Render  it to device
