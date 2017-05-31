820c5cd103b0944af51e3a2585269fe1b1f179ff
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-05-31 19:30:28 +0300

    Album Detail

---
+++ albums/src/components/AlbumDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+const AlbumDetail = (props) => {
+  return (
+      <View>
+        <Text key={props.album.title}>{props.album.title}</Text>
+      </View>)
+};
+
+export default AlbumDetail;
