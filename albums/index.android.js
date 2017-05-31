86783bc4521825d48b6f1e1b6e84db8dea88778b
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-06-01 00:52:57 +0300

    Add ScrollView Feature

--- albums/index.android.js
+++ albums/index.android.js
@@ -1,17 +1,11 @@
-// Index.android.js - place code in here for android!!!
-
-
-// Import a library to help create a componenet
-
 import React from 'react';
 import { Text, AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
 import AlbumList from './src/components/AlbumList';
 
-
 // Create a Component
 const App = () => (
-    <View>
+    <View style={{flex: 1}}>
       <Header headerText={'Albums'}/>
       <AlbumList />
     </View>
