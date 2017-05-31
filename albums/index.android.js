c6073cc75e7effb48f68f8d18e35670f76dfe46d
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-05-31 06:13:34 +0300

    Add Album List

--- albums/index.android.js
+++ albums/index.android.js
@@ -4,12 +4,17 @@
 // Import a library to help create a componenet
 
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { Text, AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
 
 // Create a Component
 const App = () => (
-    <Header headerText={'Albums'}/>
+    <View>
+      <Header headerText={'Albums'}/>
+      <AlbumList />
+    </View>
 );
 
 // Render  it to device
---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const AlbumList = () => {
+  return (
+      <View>
+        <Text>Album List!!!</Text>
+      </View>
+  )
+};
+
+
+export default AlbumList
