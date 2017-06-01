496b972096b99c19cc94bdeaf78314b9da96132d
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-06-01 12:40:31 +0300

    Linking

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, Linking } from 'react-native'
 import Card from './Card.js'
 import CardSection from './CardSection'
 import Button from './Button';
 
 const AlbumDetail = ({album}) => {
-  const {title, artist, thumbnail_image, image} = album;
+  const {title, artist, thumbnail_image, image, url} = album;
   const {headerContentStyle, headerTextStyle, thumbnailStyle, imageStyle, thumbnailContainerStyle} = styles;
 
   return (
@@ -23,7 +23,7 @@ const AlbumDetail = ({album}) => {
           <Image style={imageStyle} source={{uri: image}}/>
         </CardSection>
         <CardSection>
-          <Button onPress={() => console.log(title)}/>
+          <Button onPress={() => Linking.openURL(url)}/>
         </CardSection>
       </Card>)
 };
