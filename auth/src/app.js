c0ad497a240c9e7b0c0426a0b6e93ac630159413
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-06-01 13:51:18 +0300

    Login Form

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Header } from './components/common'
-
+import firebase from 'firebase'
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyC0HOe_ntW2mK9roDGP27UxMaKEW5Vtf74",
+      authDomain: "okk-sumbul.firebaseapp.com",
+      databaseURL: "https://okk-sumbul.firebaseio.com",
+      projectId: "okk-sumbul",
+      storageBucket: "okk-sumbul.appspot.com",
+      messagingSenderId: "1019072249963"
+    });
+  }
+
   render() {
     return (
         <View>
-          <Header headerText="Authentication" />
-          <Text>An App!</Text>
+          <Header headerText="Authentication"/>
+          <LoginForm/>
         </View>
     )
   }
